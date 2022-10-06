dd591e42d832b06270e0d3635702e37e06c066b9
Troy Radam <troy.radam@ontariotechu.net>
2022-10-06 00:25:08 -0400

    Workout function to calculate entropy of other attributes based off target variable

--- Lab Code/Lab 2/lab2_dt.py
+++ Lab Code/Lab 2/lab2_dt.py
@@ -1,5 +1,23 @@
+from tkinter.tix import COLUMN
 import pandas as pd
 import numpy as np
+
+def entropyAtt(df, attribute, targetVariable):
+    targetCol = df.loc[: , targetVariable]
+    targetColValues = targetCol.unique()
+    attCol = df.loc[:, attribute]
+    attColValues = attCol.unique()
+
+    tv_att = df[[attribute, targetVariable]]
+
+    print(tv_att)
+
+
+
+
+
+
+
 
 inputData1 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\breast-cancer-wisconsin-wLabels-train.csv"
 inputData2 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\letter-recognition-wLabels-train.csv"
@@ -16,20 +34,31 @@ userDataPath = lecData
 #add header arguement to prevent first row from being read as labels. Enable whitespace delim line for whitespace delim datasets
 df = pd.read_csv(userDataPath)
 
+columnsNamesArr = df.columns.values
 #Test Calculation For Entropy
 #Finding the Entropy of Target Variable
 
 entropyTV = 0
-targetCol = df.iloc[: , -1]
-print(targetCol.unique())
+# targetCol = df.iloc[: , -1]
+# print(targetCol.unique())
+# instances = targetCol.value_counts()
+
+# for x in instances:
+#     pi = x / len(targetCol)
+#     entropyTV += -pi*np.log2(pi)
+
+targetCol = df.loc[: , columnsNamesArr[-1]]
 instances = targetCol.value_counts()
 
 for x in instances:
     pi = x / len(targetCol)
     entropyTV += -pi*np.log2(pi)
-
 print(entropyTV)
 
-for column in range(df.shape[1] -1):
-    colTest = df.iloc[:,column]
-    print(colTest)
+# for column in range(df.shape[1] -1):
+#     colTest = df.iloc[:,column]
+#     print(colTest)
+
+#for column in columnsNamesArr[:-1]:
+
+entropyAtt(df, "Wind", columnsNamesArr[-1])
