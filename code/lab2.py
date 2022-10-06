d72caf638ce9001af218ec1d33353c37d8cc7bd7
noorbot <noor.khabbaz@ontariotechu.net>
2022-10-06 11:53:13 -0400

    getting lab 2 closer to working

--- code/lab2.py
+++ code/lab2.py
@@ -5,25 +5,33 @@ file_name = "results/letter_recognition_train_data.csv"         # <-- change fil
 
 # read from data file and save to pandas DataFrame 'data'
 #data = pd.read_csv(file_name, header = None)
-data = pd.DataFrame([[1, 2, 'A'], 
-[4, 5, 'B'], 
-[5, 8, 'C'],
-[2, 7, 'A'], 
-[5, 8, 'A'],
-[2, 5, 'B'], 
-[2, 8, 'C'],
-[4, 5, 'A'], 
-[5, 9, 'C'],
-])
+data = pd.DataFrame([ 
+['Sunny',   'Weak',   'No'], 
+['Sunny',   'Strong', 'No'],
+['Overcast','Weak',   'Yes'], 
+['Rain',    'Weak',   'Yes'],
+['Rain',    'Weak',   'Yes'], 
+['Rain',    'Strong', 'No'],
+['Overcast','Strong', 'Yes'], 
+['Sunny',   'Weak',   'No'],
+['Sunny',   'Weak',   'Yes'],
+['Rain',    "Weak",   'Yes'],
+['Sunny',   'Strong', 'Yes'],
+['Overcast','Strong', 'Yes'],
+['Overcast','Weak',   'Yes'],
+['Rain',    'Strong', 'No']
+], columns=['Outlook', 'Wind', 'Tennis'])
 
 print(data.head())
 
 # count the number of attributes
 num_attributes = data.shape[1] - 1
 num_instances = data.shape[0]
+print('\nNumber of attributes: ' + str(num_attributes))
+print('Number of instances: ' + str(num_instances))
 
 target_classes = data.iloc[:, -1].unique()
-print(target_classes)
+print('Target variable classes: ' + str(target_classes))
 num_target_classes = np.count_nonzero(target_classes)
 
 class_prop = []
@@ -40,29 +48,31 @@ for clas in target_classes:                 # find proportions
 for i in range(0, num_target_classes):      #calculate entropy of target var
     target_entropy = target_entropy - class_prop[i] * np.log2(class_prop[i])
 
-print(target_entropy)
+print('Target var entropy: ' + str(target_entropy))
 
 
 
-Attribute_Entropies = pd.DataFrame(index=np.arange(1), columns=np.arange(4 + num_target_classes))
+Attribute_Entropies = pd.DataFrame(index=np.arange(0), columns=np.arange(4 + num_target_classes))
 
 # find entropy of target variable w.r.t. each attribute
 for attribute in range(0, num_attributes):                 # for each attribute ...
     attribute_classes = data.iloc[:, attribute].unique()   # find unique attribute classes
     num_attribute_classes = np.count_nonzero(attribute_classes)
-    print('attribute: ' + str(attribute))
+    print('\nattribute: ' + str(attribute))
     print('num attribute classes: ' + str(num_attribute_classes))
     print(attribute_classes)
 
     for clas, i in zip(attribute_classes, range(0,num_attribute_classes)):                         # for each class of an attribute ...
         num_attribute_class_for_target = data.iloc[:,attribute].value_counts()[clas]               # count instances of that class
-        Attribute_Entropies.loc[attribute + i, 0] = attribute
-        Attribute_Entropies.loc[attribute + i, 1] = clas
-        Attribute_Entropies.loc[attribute + i, 2] = num_attribute_class_for_target
+        Attribute_Entropies.loc[len(Attribute_Entropies), [0, 1, 2]] = [attribute, clas, num_attribute_class_for_target]
         
         for clas_1, j in zip(target_classes, range(0,num_target_classes) ):                      # for each target variable class
-            #proppy = data.iloc[:,attribute].value_counts()[clas_1]    # number of time this attribute with this target variable class
-            #Attribute_Entropies.loc[attribute + i, 3 + j] = proppy
-            print("yeah this is where i need to fix. proppy")
+            print("\nattribute class: " + str(clas) + "      target class: "  + str(clas_1))
+            proppy = len(data[(data.iloc[:,attribute]==clas) & (data.iloc[:,-1]==clas_1)])
 
-print(Attribute_Entropies)
+            print('proppy ' + str(proppy))
+            #print(type(proppy))
+            Attribute_Entropies.loc[attribute + i, 3 + j] = proppy
+            print("yeah this is where i need to fix.")
+    print(Attribute_Entropies)
+
