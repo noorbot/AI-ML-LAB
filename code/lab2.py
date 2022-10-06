76db8aca893adb6101656c5b879f173598a4a81e
noorbot <noor.khabbaz@ontariotechu.net>
2022-10-06 12:59:07 -0400

    getting lab 2 closer to working

--- code/lab2.py
+++ code/lab2.py
@@ -51,7 +51,8 @@ for i in range(0, num_target_classes):      #calculate entropy of target var
 print('Target var entropy: ' + str(target_entropy))
 
 
-
+prev_num_attribute_classes = 0
+attr_class_entropy = 0
 Attribute_Entropies = pd.DataFrame(index=np.arange(0), columns=np.arange(4 + num_target_classes))
 
 # find entropy of target variable w.r.t. each attribute
@@ -65,14 +66,42 @@ for attribute in range(0, num_attributes):                 # for each attribute 
     for clas, i in zip(attribute_classes, range(0,num_attribute_classes)):                         # for each class of an attribute ...
         num_attribute_class_for_target = data.iloc[:,attribute].value_counts()[clas]               # count instances of that class
         Attribute_Entropies.loc[len(Attribute_Entropies), [0, 1, 2]] = [attribute, clas, num_attribute_class_for_target]
+        print("HERE")
+        print("da entropy:  " + str(attr_class_entropy))
+        attr_class_entropy = 0
         
         for clas_1, j in zip(target_classes, range(0,num_target_classes) ):                      # for each target variable class
             print("\nattribute class: " + str(clas) + "      target class: "  + str(clas_1))
             proppy = len(data[(data.iloc[:,attribute]==clas) & (data.iloc[:,-1]==clas_1)])
-
             print('proppy ' + str(proppy))
-            #print(type(proppy))
-            Attribute_Entropies.loc[attribute + i, 3 + j] = proppy
-            print("yeah this is where i need to fix.")
-    print(Attribute_Entropies)
+            Attribute_Entropies.loc[prev_num_attribute_classes + i, 3 + j] = proppy
+            if(proppy !=0):
+                attr_class_entropy = attr_class_entropy - proppy/num_attribute_class_for_target * np.log2(proppy/num_attribute_class_for_target)
+            else: attr_class_entropy = attr_class_entropy +0
+            Attribute_Entropies.iloc[prev_num_attribute_classes + i, -1] = attr_class_entropy
+           
+    prev_num_attribute_classes = num_attribute_classes
 
+print(Attribute_Entropies)
+
+
+prev_num_attribute_classes = 0
+IG = target_entropy
+Attribute_IG = pd.DataFrame(index=np.arange(0), columns=np.arange(2))
+
+
+# find information gain of each attribute
+for attribute in range(0, num_attributes):                 # for each attribute ...
+    attribute_classes = data.iloc[:, attribute].unique()   # find unique attribute classes
+    num_attribute_classes = np.count_nonzero(attribute_classes)
+    Attribute_IG.loc[len(Attribute_IG), 0] = attribute
+
+    print('num attributes: ' + str(num_attributes) + '    num classes:' + str(num_attribute_classes))
+
+    for clas, i in zip(attribute_classes, range(0,num_attribute_classes)):                 # for each attribute ...
+        IG = IG - Attribute_Entropies.iloc[prev_num_attribute_classes + i , 2] / num_instances * np.log2(Attribute_Entropies.iloc[prev_num_attribute_classes + i , 2] / num_instances)
+        Attribute_IG.iloc[attribute, -1] = IG
+
+    prev_num_attribute_classes = num_attribute_classes
+
+print(Attribute_IG)
