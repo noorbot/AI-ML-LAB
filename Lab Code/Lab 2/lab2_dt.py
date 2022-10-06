68d9f5342656d07984f9a77e3db60a00751f8fcd
Troy Radam <troy.radam@ontariotechu.net>
2022-10-05 20:57:41 -0400

    Testing getting functions for unique values

--- Lab Code/Lab 2/lab2_dt.py
+++ Lab Code/Lab 2/lab2_dt.py
@@ -1,12 +1,22 @@
 import pandas as pd
 
-inputData2 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\OG Datasets\Letter Recognition Data Set\letterRecog-train.csv"
+inputData1 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\breast-cancer-wisconsin-wLabels-train.csv"
+inputData2 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\letter-recognition-wLabels-train.csv"
+inputData3 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\ecoli-wLabels-train.csv"
+inputData4 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\agaricus-lepiota-wLabels-train.csv"
+inputData5 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\lp5-formatted-wLabels-train.csv"
 
 userDataPath = inputData2
 
 #add header arguement to prevent first row from being read as labels. Enable whitespace delim line for whitespace delim datasets
-#df = pd.read_csv(userDataPath,delim_whitespace= True, header=None)
-df = pd.read_csv(userDataPath, header=None)
+df = pd.read_csv(userDataPath)
 
 #Test Calculation For Entropy
-print(df)
+#Finding the Entropy of Target Variable
+
+entropyTV = 0
+targetCol = df.iloc[: , -1]
+print(targetCol.unique())
+instances = targetCol.value_counts()
+print(instances)
+
