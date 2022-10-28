494d02752f1bbcb3be81d86560fdad4b5925a83c
Troy Radam <troy.radam@ontariotechu.net>
2022-10-28 10:16:46 -0400

    Initialize and setup lab 3 code

---
+++ Lab Code/Lab 3/lab3_knn.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+
+
+#File paths for training datasets
+trainData1 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\wdbc_train_data.csv"
+trainData2 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\letter-recognition-wLabels-train.csv"
+trainData3 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\ecoli_train_data.csv"
+trainData4 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\agaricus-lepiota-wLabels-train.csv"
+trainData5 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\lp5_train_data.csv"
+
+#File paths for testing datasets
+testData1 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\wdbc_test_data.csv"
+testData2 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\letter-recognition-wLabels-test.csv"
+testData3 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\ecoli_test_data.csv"
+testData4 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\agaricus-lepiota-wLabels-test.csv"
+testData5 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\lp5_test_data.csv"
+
+#Debugging datasets
+lecData = r"D:\Users\radam\Desktop\lecData.csv"
+test = r"D:\Users\radam\Desktop\wdbc_test_data.csv"
+train = r"D:\Users\radam\Desktop\wdbc_train_data.csv"
+
+#Set dataset to build decision tree from and test
+userDataPath = trainData1
+testDataPath = testData1
+
+#For debugging and testing the program
+#userDataPath = lecData
+# userDataPath = train
+# testDataPath = test
+
+#Create dataframes using csv files
+df = pd.read_csv(userDataPath)
+testDF = pd.read_csv(testDataPath)
+
+#Obtain attributes of the data set and set target variable to last value of array
+columnsNamesArr = df.columns.values
+targetAttribute = columnsNamesArr[-1]
