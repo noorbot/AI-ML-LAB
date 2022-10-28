b8ce31bfdd4d943162b84788e09e126e07fb395a
Troy Radam <troy.radam@ontariotechu.net>
2022-10-28 11:09:43 -0400

    Setup of Preliminary Functions

--- Lab Code/Lab 3/lab3_knn.py
+++ Lab Code/Lab 3/lab3_knn.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 
+def subsetDf(df, attribute1, attribute2, targVal):
+    return df.loc[:, [attribute1, attribute2, targVal]]
+
+def calculateED(rowA, rowB):
+    
+    
+
+    return
+
 
 
 #File paths for training datasets
@@ -23,18 +32,24 @@ test = r"D:\Users\radam\Desktop\wdbc_test_data.csv"
 train = r"D:\Users\radam\Desktop\wdbc_train_data.csv"
 
 #Set dataset to build decision tree from and test
-userDataPath = trainData1
-testDataPath = testData1
+# userDataPath = trainData1
+# testDataPath = testData1
 
 #For debugging and testing the program
-#userDataPath = lecData
+userDataPath = lecData
 # userDataPath = train
 # testDataPath = test
 
 #Create dataframes using csv files
 df = pd.read_csv(userDataPath)
-testDF = pd.read_csv(testDataPath)
+#testDF = pd.read_csv(testDataPath)
 
 #Obtain attributes of the data set and set target variable to last value of array
 columnsNamesArr = df.columns.values
 targetAttribute = columnsNamesArr[-1]
+
+k = 2;
+
+
+print(subsetDf(df, 'Outlook', 'Temperature', targetAttribute))
+
