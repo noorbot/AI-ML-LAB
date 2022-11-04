c522f1a18927ddfc1ea38225c93b9cb6ef18473a
Troy Radam <troy.radam@ontariotechu.net>
2022-11-04 19:24:54 -0400

    Working KNN (I think)

--- Lab Code/Lab 3/lab3_knn.py
+++ Lab Code/Lab 3/lab3_knn.py
@@ -9,42 +9,75 @@ def calculateED(rowTrain, rowTest):
     elementSum = 0
 
     for index, x1 in rowTrain.items():
-        x2 = rowTest.iloc[index]
+        x2 = rowTest.loc[index]
         elementSum += ((x1-x2)**2)
 
     dist = np.sqrt(elementSum)
 
     return  dist
 
-def knn(trainDF, testDF, k):
+def knn(testRow, trainDF, k):
 
     euclidDist = []
-    # attributes = trainDF.keys()
-    # distance = 0
+    trainLabels = trainDF.keys()
+    trainTV = trainLabels[-1]
+    test = testRow.drop(labels=['Doggy'])
 
-    # testQRow = 0
-    
-    # testQ = trainDF.iloc[testQRow]
-    #newDF = trainDF.drop([testQRow]).reset_index(drop=True)
-    testRow = testDF.iloc[0]
+    for indexTrain, rowTrain in trainDF.iterrows():
+        testClassification = rowTrain.iloc[-1]
+        rowTrain = rowTrain.drop(labels=[trainTV])
+        euclidDist.append(tuple([testClassification, calculateED(rowTrain,test)]))
 
-    for index, row in trainDF.iterrows():
-        #testRow = testDF.iloc[index]
-        euclidDist.append(calculateED(testRow,row))
-
-    euclidDist.sort()
+    euclidDist.sort(key=lambda a: a[1])
     neighbours = []
     for i in range(k):
         neighbours.append(euclidDist[i])
-    # for index, row in trainDF.iterrows():
-    #     for index2, value in row.items():
-    #         print(f"Index : {index2}, Value : {value}")
-    #         distance += (value - awddwad)**2
-
-    #     euclidDist.update(distance*(1/2))
 
     return neighbours
 
+def findNeighbour(neighbour):
+    df = pd.DataFrame(neighbour)
+    counts = df[0].value_counts().idxmax()
+    return counts
+
+# def knn(trainDF, testDF, k):
+
+#     euclidDist = []
+#     # attributes = trainDF.keys()
+#     # distance = 0
+
+#     # testQRow = 0
+    
+#     # testQ = trainDF.iloc[testQRow]
+#     #newDF = trainDF.drop([testQRow]).reset_index(drop=True)
+
+#     #testRow = testDF.iloc[0]
+
+#     for indexTest, rowTest in testDF.iterrows():
+#         testRow = testDF.iloc[indexTest]
+#         for indexTrain, rowTrain in trainDF.iterrows():
+#             euclidDist.append(calculateED(testRow,rowTrain))
+
+#     euclidDist.sort()
+#     neighbours = []
+
+#     for i in range(k):
+#         neighbours.append(euclidDist[i])
+#     # for index, row in trainDF.iterrows():
+#     #     for index2, value in row.items():
+#     #         print(f"Index : {index2}, Value : {value}")
+#     #         distance += (value - awddwad)**2
+
+#     #     euclidDist.update(distance*(1/2))
+
+#     return neighbours
+
+# Make a classification prediction with neighbors
+def predict_classification(train, test_row, num_neighbors):
+	neighbors = knn(train, test_row, num_neighbors)
+	output_values = [row[-1] for row in neighbors]
+	prediction = max(set(output_values), key=output_values.count)
+	return prediction
 
 #File paths for training datasets
 trainData1 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\wdbc_train_data.csv"
@@ -85,3 +118,39 @@ k = 2;
 
 #calculateED(trainDF, testDF)
 
+# dataset = [[2.7810836,2.550537003,0],
+# 	[1.465489372,2.362125076,0],
+# 	[3.396561688,4.400293529,0],
+# 	[1.38807019,1.850220317,0],
+# 	[3.06407232,3.005305973,0],
+# 	[7.627531214,2.759262235,1],
+# 	[5.332441248,2.088626775,1],
+# 	[6.922596716,1.77106367,1],
+# 	[8.675418651,-0.242068655,1],
+# 	[7.673756466,3.508563011,1]]
+
+dataset = [[2.7810836,2.550537003,0,'A'],
+	[1.465489372,2.362125076,0,'A'],
+	[3.396561688,4.400293529,0,'B'],
+	[1.38807019,1.850220317,0,'B'],
+	[3.06407232,3.005305973,0,'B'],
+	[7.627531214,2.759262235,1,'A'],
+	[5.332441248,2.088626775,1,'A'],
+	[6.922596716,1.77106367,1,'B'],
+	[8.675418651,-0.242068655,1,'B'],
+	[7.673756466,3.508563011,1,'B']]
+
+dataset2 = [3.396561688,4.400293529,0,'B']
+
+dfTEST = pd.DataFrame(data=dataset, columns=['Alfred', 'Bobert', 'Charlie', 'Doggy'])
+# row0 = dfTEST.iloc[0]
+dfTEST2 = pd.Series(dataset2,index=['Alfred', 'Bobert', 'Charlie', 'Doggy'])
+    
+# for index, row in dfTEST.iterrows():
+#     print(calculateED(row0, row))
+# testRow = dfTEST.iloc[0]
+# testlabels = testRow.keys()
+# print(testlabels[-1])
+testNeigh = knn(dfTEST2, dfTEST, 3)
+classifc = findNeighbour(testNeigh)
+print(classifc)
