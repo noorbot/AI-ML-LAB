b510c4f6f04d0ecd4da5e3c862950d3f012995b3
Troy Radam <troy.radam@ontariotechu.net>
2022-11-04 19:58:11 -0400

    Working?

--- Lab Code/Lab 3/lab3_knn.py
+++ Lab Code/Lab 3/lab3_knn.py
@@ -1,5 +1,7 @@
+from symbol import tfpdef
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 # def subsetDf(df, attribute1, attribute2, targVal):
 #     return df.loc[:, [attribute1, attribute2, targVal]]
@@ -21,56 +23,27 @@ def knn(testRow, trainDF, k):
     euclidDist = []
     trainLabels = trainDF.keys()
     trainTV = trainLabels[-1]
-    test = testRow.drop(labels=['Doggy'])
+    #testRow = testRow.drop(labels=['Doggy'])
 
     for indexTrain, rowTrain in trainDF.iterrows():
         testClassification = rowTrain.iloc[-1]
         rowTrain = rowTrain.drop(labels=[trainTV])
-        euclidDist.append(tuple([testClassification, calculateED(rowTrain,test)]))
+        euclidDist.append(tuple([testClassification, calculateED(rowTrain,testRow)]))
 
     euclidDist.sort(key=lambda a: a[1])
-    neighbours = []
+    neighbourHood = []
     for i in range(k):
-        neighbours.append(euclidDist[i])
+        neighbourHood.append(euclidDist[i])
 
-    return neighbours
+    dfNeighbour = pd.DataFrame(neighbourHood)
+    neighbour = dfNeighbour[0].value_counts().idxmax()
 
-def findNeighbour(neighbour):
-    df = pd.DataFrame(neighbour)
-    counts = df[0].value_counts().idxmax()
-    return counts
+    return neighbour
 
-# def knn(trainDF, testDF, k):
-
-#     euclidDist = []
-#     # attributes = trainDF.keys()
-#     # distance = 0
-
-#     # testQRow = 0
-    
-#     # testQ = trainDF.iloc[testQRow]
-#     #newDF = trainDF.drop([testQRow]).reset_index(drop=True)
-
-#     #testRow = testDF.iloc[0]
-
-#     for indexTest, rowTest in testDF.iterrows():
-#         testRow = testDF.iloc[indexTest]
-#         for indexTrain, rowTrain in trainDF.iterrows():
-#             euclidDist.append(calculateED(testRow,rowTrain))
-
-#     euclidDist.sort()
-#     neighbours = []
-
-#     for i in range(k):
-#         neighbours.append(euclidDist[i])
-#     # for index, row in trainDF.iterrows():
-#     #     for index2, value in row.items():
-#     #         print(f"Index : {index2}, Value : {value}")
-#     #         distance += (value - awddwad)**2
-
-#     #     euclidDist.update(distance*(1/2))
-
-#     return neighbours
+# def findNeighbour(neighbour):
+#     df = pd.DataFrame(neighbour)
+#     counts = df[0].value_counts().idxmax()
+#     return counts
 
 # Make a classification prediction with neighbors
 def predict_classification(train, test_row, num_neighbors):
@@ -78,6 +51,27 @@ def predict_classification(train, test_row, num_neighbors):
 	output_values = [row[-1] for row in neighbors]
 	prediction = max(set(output_values), key=output_values.count)
 	return prediction
+
+def evaluate(trainDF, testDF, k): # method to feed in test instances and calculate algorithm accuracy
+    correct_predict = 0
+    wrong_predict = 0
+
+    testLabels = testDF.keys()
+    testTV = testLabels[-1]
+
+    for index, row in tqdm(testDF.iterrows()):
+        rowQ = row
+        testClassification = rowQ.iloc[-1]
+        rowQ = rowQ.drop(labels=[testTV])
+        neighbour = knn(rowQ, trainDF, k)
+
+        if neighbour == testClassification:
+            correct_predict += 1 #increase correct count
+        else:
+            wrong_predict += 1 #increase incorrect count
+
+    accuracy = correct_predict / (correct_predict + wrong_predict) #calculating accuracy
+    return accuracy
 
 #File paths for training datasets
 trainData1 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\wdbc_train_data.csv"
@@ -99,22 +93,22 @@ test = r"D:\Users\radam\Desktop\wdbc_test_data.csv"
 train = r"D:\Users\radam\Desktop\wdbc_train_data.csv"
 
 #Set dataset to build decision tree from and test
-userDataPath = trainData2
-testDataPath = testData2
+trainDataPath = trainData5
+testDataPath = testData5
 
 #For debugging and testing the program
 # userDataPath = train
 # testDataPath = test
 
 #Create dataframes using csv files
-trainDF = pd.read_csv(userDataPath)
+trainDF = pd.read_csv(trainDataPath)
 testDF = pd.read_csv(testDataPath)
 
 #Obtain attributes of the data set and set target variable to last value of array
 columnsNamesArr = trainDF.columns.values
 targetAttribute = columnsNamesArr[-1]
 
-k = 2;
+k = 8;
 
 #calculateED(trainDF, testDF)
 
@@ -140,17 +134,19 @@ dataset = [[2.7810836,2.550537003,0,'A'],
 	[8.675418651,-0.242068655,1,'B'],
 	[7.673756466,3.508563011,1,'B']]
 
-dataset2 = [3.396561688,4.400293529,0,'B']
+dataset2 = [[2.7810836,2.550537003,0,'A'],
+	[1.465489372,2.362125076,0,'A'],
+	[3.396561688,4.400293529,0,'B'],
+	[1.38807019,1.850220317,0,'B'],
+	[3.06407232,3.005305973,0,'B'],
+	[7.627531214,2.759262235,1,'A'],
+	[5.332441248,2.088626775,1,'A'],
+	[6.922596716,1.77106367,1,'B'],
+	[8.675418651,-0.242068655,1,'B'],
+	[7.673756466,3.508563011,1,'B']]
 
 dfTEST = pd.DataFrame(data=dataset, columns=['Alfred', 'Bobert', 'Charlie', 'Doggy'])
 # row0 = dfTEST.iloc[0]
-dfTEST2 = pd.Series(dataset2,index=['Alfred', 'Bobert', 'Charlie', 'Doggy'])
+dfTEST2 = pd.DataFrame(data=dataset2,columns=['Alfred', 'Bobert', 'Charlie', 'Doggy'])
     
-# for index, row in dfTEST.iterrows():
-#     print(calculateED(row0, row))
-# testRow = dfTEST.iloc[0]
-# testlabels = testRow.keys()
-# print(testlabels[-1])
-testNeigh = knn(dfTEST2, dfTEST, 3)
-classifc = findNeighbour(testNeigh)
-print(classifc)
+print('K = ', str(k), ', ', evaluate(trainDF, testDF, k))
