5b1a6ca4f2a95194403213fbf8f5c38f8e4bdf55
Hotchnerz <troy.radam@ontariotechu.net>
2022-11-25 10:47:13 -0500

    Intialize and setup lab5

---
+++ Lab Code/Lab 5/lab5_lib.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+#File paths for training datasets
+trainData1 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\wdbc_train_data.csv"
+trainData2 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\letter-recognition-wLabels-train.csv"
+trainData3 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\ecoli_train_data.csv"
+trainData4 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\agaricus-lepiota-wLabels-train.csv"
+trainData5 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\lp5_train_data.csv"
+
+#File paths for testing datasets
+testData1 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\wdbc_test_data.csv"
+testData2 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\letter-recognition-wLabels-test.csv"
+testData3 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\ecoli_test_data.csv"
+testData4 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\agaricus-lepiota-wLabels-test.csv"
+testData5 = r"D:\Users\radam\Desktop\ENGR 3150U Lab Files\AI-ML-LAB\Datasets\Lab 2 Datasets\lp5_test_data.csv"
+
+#Debugging datasets
+lecData = r"D:\Users\radam\Desktop\lecData.csv"
+lecDataTest = r"D:\Users\radam\Desktop\lecDataTest.csv"
+testData = r"D:\Users\radam\Desktop\test.csv"
+test = r"D:\Users\radam\Desktop\wdbc_test_data.csv"
+train = r"D:\Users\radam\Desktop\wdbc_train_data.csv"
+
+#Set dataset to build decision tree from and test
+trainDataPath = trainData4
+testDataPath = testData4
+
+#For debugging and testing the program
+# userDataPath = lecData
+# #testDataPath = testData
+# testDataPath= lecDataTest
+
+#Debug dataframes using csv files
+# trainDF = pd.read_csv(userDataPath)
+# testDF = pd.read_csv(testDataPath)
+
+#Create dataframes using csv files
+trainDF = pd.read_csv(trainDataPath)
+testDF = pd.read_csv(testDataPath)
