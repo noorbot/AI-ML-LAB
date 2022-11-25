581a66bdc2e87bfe85d52f0eef2ae2d1dd6af319
noorbot <noor.khabbaz@ontariotechu.net>
2022-11-25 11:22:38 -0500

    in progress

--- code/lab5.py
+++ code/lab5.py
@@ -1,27 +1,22 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
 
 
-train_file_name = "results/mushroom_train_data.csv"     # <-- change file names to match data set (use binned data for numerical datasets)
-test_file_name = "results/mushroom_test_data.csv"
+data_file_name = "data/lab5/ecoli.csv"     # <-- change file name to match data set
 
-train_data = pd.read_csv(train_file_name, header = None)          # read from data file and save to pandas DataFrames
-test_data = pd.read_csv(test_file_name, header=None)
+data = pd.read_csv(data_file_name, header = None)          # read from data file and save to pandas DataFrames
 
-target_column = train_data.iloc[:,-1]
-features = 
+target_column = data.iloc[:,-1]
+features = data.iloc[:, :-1]
 
-X = 
-y = 
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-clf.fit(X, y)
+mlp = MLPClassifier(hidden_layer_sizes=(8,8,8), activation='relu', solver='adam', max_iter=500)
+mlp.fit(features, target_column)
 MLPClassifier(alpha=1e-05, hidden_layer_sizes=(5, 2), random_state=1,solver='lbfgs')
 
 # Compute 5-fold cross-validation scores: cv_scores
-cv_scores = cross_val_score(clf, X, y, cv=5)
+cv_scores = cross_val_score(mlp, features, target_column, cv=5)
 
 # Print the 5-fold cross-validation scores
 print(cv_scores)
