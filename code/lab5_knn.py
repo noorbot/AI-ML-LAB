b46bc3d21f1c89c5c6bcc07d2877e62c86aa506e
noorbot <noor.khabbaz@ontariotechu.net>
2022-11-25 12:01:46 -0500

    knn working woo

--- code/lab5_knn.py
+++ code/lab5_knn.py
@@ -1,4 +1,4 @@
-from sklearn.neural_network import MLPClassifier
+from sklearn import neighbors
 from sklearn.model_selection import cross_val_score
 import numpy as np
 import pandas as pd
@@ -12,12 +12,12 @@ target_column = data.iloc[:,-1]
 features = data.iloc[:, :-1]
 # data[features] = data[features]/data[features].max()
 
-mlp = MLPClassifier(hidden_layer_sizes=(16,16), activation='relu', solver='adam', max_iter=1000)
-mlp.fit(features, target_column)
+knn = neighbors.KNeighborsClassifier(n_neighbors=5)
+knn.fit(features, target_column)
 
 
 # Compute 5-fold cross-validation scores: cv_scores
-cv_scores = cross_val_score(mlp, features, target_column, cv=5)
+cv_scores = cross_val_score(knn, features, target_column, cv=5)
 
 # Print the 5-fold cross-validation scores
 print(cv_scores)
